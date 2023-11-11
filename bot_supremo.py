ad93ff0ce9997cf48aa29139f216f181f7d6ace7
Julio Mol <52005405+juliomol1777@users.noreply.github.com>
2023-11-11 03:30:01 -0300

    actualizo

--- bot_supremo.py
+++ bot_supremo.py
@@ -2,13 +2,13 @@ import telegram
 import telegram.ext
 import openai
 
-# Configura el token de acceso de tu bot de Telegram
+#token de acceso al bot de Telegram
 bot_token = "aqui va el token"
 
-# Configura tu clave de API de GPT-3
+#clave de API de GPT-3
 openai.api_key = "aqui va el token"
 
-# Define una función para manejar los mensajes que reciba tu bot
+#maneja los mensajes que reciba el bot
 def handle_message(update, context):
     # Obtiene el texto del mensaje recibido
     message_text = update.message.text
@@ -21,16 +21,16 @@ def handle_message(update, context):
         temperature=0.2
     )
     
-    # Obtiene la respuesta generada por la API de GPT-3
+    #respuesta generada por la API de GPT-3
     response_text = response.choices[0].text.strip()
     
-    # Envía la respuesta a Telegram
+    #Envía la respuesta a Telegram
     update.message.reply_text(response_text)
 
-# Crea un "telegram.ext.Updater" y registra la función "handle_message" como manejador de mensajes
+#Crea un "telegram.ext.Updater" y registra la función "handle_message" como manejador de mensajes
 updater = telegram.ext.Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 
-# Inicia el bot
+#Inicia el bot
 updater.start_polling()
